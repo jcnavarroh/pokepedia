a786ec9c6e978e55351b449d19ed386ecd74f0f8
Juan Navarro <junavarro2@poligran.edu.co>
2020-09-24 20:47:39 -0500

    Creación de estructura de componentes

--- Pokepedia/src/app/app.module.ts
+++ Pokepedia/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    CardsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
