f045c76e17cf9f0c4c2599efc023c0c5e882c5a8
JapNeitor <jorgealbertoperea@gmail.com>
2020-09-27 10:03:39 -0500

    Configuracion Inicial del routing
    Congiguracion de las rutas a los 2 componentes principales 'HomeComponent' y 'CardsComponent'

--- Pokepedia/src/app/app-routing.module.ts
+++ Pokepedia/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardsComponent } from './components/cards/cards.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'card/:id', component: CardsComponent},
+  {path:'*',  redirectTo: '', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
