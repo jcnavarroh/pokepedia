0bf2988516219d04e7155b61872af491b9e7dac5
Edwin Quevedo <edwinqs27@gmail.com>
2020-09-27 12:22:28 -0500

    Agregada Galeria de las versiones del Pokemon
    La galeria se llena con una arreglo de objetos que se construirá el atributo 'versions' en la respuesta de la API, por ahora solo usa informacion de ejemplo e imagenes que son solo placeholder

--- Pokepedia/src/app/components/cards/cards.component.ts
+++ Pokepedia/src/app/components/cards/cards.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
+  versiones = [{ nombre: 'version 1', url: 'https://www.iconfinder.com/data/icons/basic-ui-6/40/Asset_12-512.png' },
+  { nombre: 'version 2', url: 'https://www.iconfinder.com/data/icons/basic-ui-6/40/Asset_12-512.png' },
+  { nombre: 'version 3', url: 'https://www.iconfinder.com/data/icons/basic-ui-6/40/Asset_12-512.png' },
+  { nombre: 'version 4', url: 'https://www.iconfinder.com/data/icons/basic-ui-6/40/Asset_12-512.png' },
+  { nombre: 'version 5', url: 'https://www.iconfinder.com/data/icons/basic-ui-6/40/Asset_12-512.png' },
+  { nombre: 'version 6', url: 'https://www.iconfinder.com/data/icons/basic-ui-6/40/Asset_12-512.png' },
+  { nombre: 'version 7', url: 'https://www.iconfinder.com/data/icons/basic-ui-6/40/Asset_12-512.png' }]
+
   constructor() { }
 
   ngOnInit(): void {
