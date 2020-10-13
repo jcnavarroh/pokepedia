47f98f1edac607a5088e67b34a6d6f8c38543bce
claudia77711 <clace79@gmail.com>
2020-10-13 18:02:43 -0500

    Se agrego menu en el navbar y se conector buscador con el app-routing

--- Pokepedia/src/app/app-routing.module.ts
+++ Pokepedia/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
 import { CardsComponent } from './components/cards/cards.component';
 import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 const APP_ROUTES: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'card/:id', component: CardsComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'search/:termino', component: SearchComponent},
   {path: '**',  redirectTo: 'home', pathMatch:'full'}
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
-
 
 
