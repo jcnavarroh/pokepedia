9bce1798a1f8ca7648a3253a71597e9ec1fc4ddf
Juan Navarro <junavarro2@poligran.edu.co>
2020-10-13 20:27:47 -0500

    Implementación final, corrección error buscador

--- Pokepedia/src/app/components/search/search.component.ts
+++ Pokepedia/src/app/components/search/search.component.ts
@@ -11,21 +11,42 @@ import { ActivatedRoute } from '@angular/router';
 export class SearchComponent implements OnInit {
 
   pokemones = [];
-  filterPokemon = [];
+  terminoBusqueda:string = "";
+  filterPokemon: any [] = [];
 
 
   constructor( private pokeapi: PokeapiService,
                public route:ActivatedRoute ) {
 
-    this.route.params.subscribe( params => {
-      this.filterPokemon [0] = params;
+    this.route.params.subscribe( (params: any) => {
+      this.terminoBusqueda = params['termino'];
+      console.log("Parametros de la URL", this.terminoBusqueda);
+      this.buscarPokemon();
     })
 
     this.pokeapi.getPokemonList().subscribe((data: any) => {
       console.log (data.results);
       this.pokemones = data.results;
-
     });
+  }
+
+  public buscarPokemon(){
+    console.log("Se disparo función de busqueda")
+    var termino:string = this.terminoBusqueda;
+    this.filterPokemon = [];
+
+    console.log("Nombre de pokemon termino de busqueda:", termino)
+
+    for (const poke of this.pokemones){
+      var pokeName = poke.name;
+
+      console.log("Nombre de pokemon del ciclo:", pokeName)
+
+      if(pokeName.includes(termino)){
+        this.filterPokemon.push(poke);
+        console.log(this.filterPokemon)
+      }
+    }
   }
 
   ngOnInit(): void {
