7ffbf825ebcb1464f14a6f0772015dd13faa5747
Juan Navarro <junavarro2@poligran.edu.co>
2020-10-13 19:02:19 -0500

    corrección error caracter invisible componente buscar

--- Pokepedia/src/app/components/search/search.component.ts
+++ Pokepedia/src/app/components/search/search.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from 'src/app/services/pokeapi.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  pokemones = [];
+  filterPokemon = [];
+
+
+  constructor( private pokeapi: PokeapiService,
+               public route:ActivatedRoute ) {
+
+    this.route.params.subscribe( params => {
+      this.filterPokemon [0] = params;
+    })
+
+    this.pokeapi.getPokemonList().subscribe((data: any) => {
+      console.log (data.results);
+      this.pokemones = data.results;
+
+    });
+  }
 
   ngOnInit(): void {
+
   }
 
 }
